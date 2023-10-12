155e5e27054ff13561397923919e59fb27b5d4e2
AnandMoorthy <anandmoorthyj>
2023-10-12 13:18:19 +0530

    Updates

---
+++ precache-manifest.e3c388f44ed2ba94410c02fae05bc255.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec54114f510c61f921be940bff08a51f",
+    "url": "/devstory/index.html"
+  },
+  {
+    "revision": "bfac38bec7461f64e250",
+    "url": "/devstory/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "1c27173ffc8193159b4b",
+    "url": "/devstory/static/css/main.c610955d.chunk.css"
+  },
+  {
+    "revision": "bfac38bec7461f64e250",
+    "url": "/devstory/static/js/2.a25f7f00.chunk.js"
+  },
+  {
+    "revision": "1c27173ffc8193159b4b",
+    "url": "/devstory/static/js/main.252682d5.chunk.js"
+  },
+  {
+    "revision": "1548f1393910052cff39",
+    "url": "/devstory/static/js/runtime-main.c70dd503.js"
+  },
+  {
+    "revision": "3bda9e9305636800f4b9084fce6f349a",
+    "url": "/devstory/static/media/DevStory.3bda9e93.svg"
+  }
+]);
